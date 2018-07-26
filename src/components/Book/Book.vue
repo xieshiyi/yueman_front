e7f446b2ddf96c1c18baa44af9f8efba26fc80f7
谢诗意 <xieshiyi7@gmail.com>
2018-07-26 18:07:11 +0800

    添加书籍的标签

--- src/components/Book/Book.vue
+++ src/components/Book/Book.vue
@@ -2,6 +2,9 @@
     <div class="book">
       <img v-lazy="item.cover_image" class="book_image" />
       <span class="tit">{{item.name}}</span>
+      <p class="tags">
+        <span v-for="(tag, index) in item.tags" :key="index">{{tag}}</span>
+      </p>
     </div>
 </template>
 <script>
